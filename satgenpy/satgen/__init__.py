48848c6fcc091f0b59e3d7e87a45f292170ce466
Lei Yuan <lyuan@telesat.com>
2025-10-28 10:49:45 -0400

    Package satgenpy to be Pypi ready

--- satgenpy/satgen/__init__.py
+++ satgenpy/satgen/__init__.py
@@ -6,3 +6,10 @@ from .dynamic_state import *
 from .description import *
 from .post_analysis import *
 from .distance_tools import *
+
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("satgenpy")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
